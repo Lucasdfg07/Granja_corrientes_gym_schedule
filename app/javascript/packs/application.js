84d47fa2c2d43c8d487c7b2d649611ad11a70513
Lucas Siqueira <lucassiqueirafernandes07@gmail.com>
2021-04-26 15:45:33 -0300

    Setting better calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+
+import "moment/locale/ja"
+import "tempusdominus-bootstrap-4"
 
 Rails.start()
 Turbolinks.start()
